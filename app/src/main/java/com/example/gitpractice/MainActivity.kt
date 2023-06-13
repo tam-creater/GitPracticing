fb3b055aab069dce495b71e63b80389c344ad3c3
tam-creater <tamshinj@fsi.co.jp>
2023-06-13 14:48:00 +0900

    Subにテキストを追加

--- app/src/main/java/com/example/gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.gitpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun change(view: View) {
+        val intent = Intent(this@MainActivity, SubActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/gitpractice/SubActivity.kt
+++ app/src/main/java/com/example/gitpractice/SubActivity.kt
@@ -1,11 +1,18 @@
 package com.example.gitpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
+    }
+
+    private fun changeSecond(view: View) {
+        val intent = Intent(this@SubActivity, MainActivity::class.java)
+        startActivity(intent)
     }
 }
